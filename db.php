23056be667e5f6142e779f17c59c2254e28e6f2a
Mashrafi271 <mashrafi.reja@northsouth.edu>
2025-03-14 00:13:27 +0600

    learned how to insert values into a table using php from the backend

--- db.php
+++ db.php
@@ -6,23 +6,31 @@ $db_server = "localhost"; //holds the name of the server
 $db_user = "root";
 $db_pass = "";
 $db_name = "test"; //name of the database, confirm that its the same as the db you will be using
-$conn = ""; //connection variable, holds connection status I think
+//$conn = ""; //connection variable, holds connection status I think
 
-//to establish a connection to the databse, set value of connection variable as such:
-$conn = mysqli_connect( //mysqli_connect() returns 1 or 0
-    $db_server,
-    $db_user,
-    $db_pass,
-    $db_name
-);
+
+//we dont want user to see any ugly error messages upon failure to connect to db for example in this case, so we encase the following code in a try catch block:
+try{
+    //to establish a connection to the databse, set value of connection variable as such:
+    $conn = mysqli_connect( //mysqli_connect() returns 1 or 0
+        $db_server,
+        $db_user,
+        $db_pass,
+        $db_name
+    );
 //add all the variables you initialized above, as parameters for the connection function
 //if u establish a successful conneciton, this $conn variable represents an object
-
-if ($conn) {
-    echo "Bluetooth device connected successfully<br>";
-} else {
+} catch(mysqli_sql_exception){
     echo "Fix ur code lil bro u are not that good<br>";
 }
 
-mysqli_close($conn);
+/*
+if ($conn) {
+    echo "Bluetooth device connected successfully<br>";
+} user doesnt really need to know if db has connected, we are not keeping this for now for testing purposes. showing only error message wil suffice ofc in shaa Allah
+*/
+
+//mysqli_close($conn); /do not close this here, sql queries in other files wont work
+
+//this php file is now complete
 ?>
--- index.php
+++ index.php
@@ -6,7 +6,34 @@
     variable names start with a $ sign
 */
 
-echo "test r";
+//include any files that this file will work with at the top here, in this php block:
+include("db.php");
+echo "testttttt<br>";
+
+//your sql queries go here, between include db and close conn
+$sql = "INSERT INTO users (user, password)
+        VALUES ('Mash', '123')"; //query enclosed in double quotes and statement concluded with a semicolon, then wrapped with mysqli_query function
+
+try {
+    mysqli_query($conn, $sql); //intelephense extension might give an error cause we r including this from another file. u can ignore this or turn off intelephense
+    echo "user is now registered<br>";
+} catch (mysqli_sql_exception) {
+    echo "could not register user<br>"; //NOTE: THIS WILL ALWAYS TRIGGER UNLESS YOU CHANGE THE VAK=LUES AND LET THE SERVER ADD A NEW ROW TO YOUR USERS TABLE, DONT BOTHER TRYING TO DEBUG THIS FOR HALF AN HOUR RAHHHHHH
+}
+
+//query two:
+$sql = "INSERT INTO users (user, password)
+VALUES ('Huhh', '153')"; //query enclosed in double quotes and statement concluded with a semicolon, then wrapped with mysqli_query function
+
+try {
+    mysqli_query($conn, $sql); //intelephense extension might give an error cause we r including this from another file. u can ignore this or turn off intelephense
+    echo "user is now registered<br>";
+} catch (mysqli_sql_exception) {
+    echo "could not register user<br>";
+}
+
+mysqli_close($conn); //intelephense extension might give an error cause we r including this from another file. u can ignore this or turn off intelephense
+
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +61,9 @@ echo "test r";
     <a href="http://localhost/QUICKFIX/db.php">
         <button>Test DB</button>
     </a>
+    <br> <!-- creates a new line after the series of test buttons -->
+
+
 </body>
 
 </html>
