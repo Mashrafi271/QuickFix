dba66a961d8a55cb6ebaf62e5fd1e3f783df5045
Abdullahil Azmi <abdullahilazmi@gmail.com>
2025-04-03 23:10:10 +0600

    Login elements.
    Added a username login form and a password input so that users may login with their usernames and passwords. More work remains to be done with respect to how it will work with the database.

--- home.php
+++ home.php
@@ -9,9 +9,9 @@ include("functions.php");
     <p>Welcome to the home page!</p>
     <form method="POST" action="">
         <label>Username:</label>
-        <input type="text" name="username" value="User" required><br>
+        <input type="text" name="username" value="User" required style="display: block; margin-bottom: 10px;"><br>
         <label>Password:</label>
-        <input type="text" name="password" required><br>
+        <input type="text" name="password" required style="display: block;"><br>
         <input type="submit" value="Login">
     </form>
 </div>
